2572797dcfa9b9178e9b0b4ac47f564fd174390a
LAKZA <116528472+XseenPro@users.noreply.github.com>
2024-05-31 14:57:39 +0700

    Delete project_pos directory

--- project_pos/admin/kasir_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../koneksi.php';
-$id = $_GET['id'];
-$data = mysqli_query($koneksi, "select * from kasir where kasir_id='$id'");
-$d = mysqli_fetch_assoc($data);
-$foto = $d['kasir_foto'];
-unlink("../gambar/kasir/$foto");
-mysqli_query($koneksi, "delete from kasir where kasir_id='$id'");
-header("location:kasir.php");
